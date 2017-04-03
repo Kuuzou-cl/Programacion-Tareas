7b0bef01f3784dec466da4eca21b21317565dd00
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2017-04-03 17:39:54 -0300

    Add files via upload

---
+++ video3/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.progra.video3;
+
+/**
+ *
+ * @author kuuzou
+ */
+public class Test {
+    
+    public static void main(String [] args){
+    
+        Alumno alum=new Alumno();
+        System.out.println(alum.sumar(1));
+    }
+    
+}
